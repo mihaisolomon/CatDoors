4ae1f73a8257899afa7bdb68e476ab3e26dd6072
Mihai Solomon <mihai.c.solomon@gmail.com>
2019-04-28 18:14:56 +0300

    Acurate location data

--- CatDoors/ViewController.swift
+++ CatDoors/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 import Alamofire;
+import MapKit;
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var gpsCoordinates: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryCodeLabel: UILabel!
+    @IBOutlet weak var streetLabel: UILabel!
+    @IBOutlet weak var localtionNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +26,14 @@ class ViewController: UIViewController {
     }
     
     @IBAction func lockDoor(_ sender: Any) {
-        AF.request("http://abdulrahmanhu.com/door-lock", method: .get)
+        AF.request("http://abdulrahmanhu.com:3000/door-lock", method: .get)
             .responseJSON { response in
                 debugPrint(response)
         }
     }
     
     @IBAction func openDoor(_ sender: Any) {
-        AF.request("http://abdulrahmanhu.com/door-open", method: .get)
+        AF.request("http://abdulrahmanhu.com:3000/door-open", method: .get)
             .responseJSON { response in
                 debugPrint(response)
         }
@@ -42,10 +48,58 @@ class ViewController: UIViewController {
                     let json = String(data: data, encoding: String.Encoding.utf8)
                     //print("Failure Response: \(json)")
                     
+                    let coordinatesArr = json?.components(separatedBy: ", ");
+                    
+                    let num = (coordinatesArr?[1] as! NSString).floatValue;
+                    let num2 = (coordinatesArr?[0] as! NSString).floatValue;
+                    
+                    let geoCoder = CLGeocoder()
+                    let location = CLLocation(latitude: CLLocationDegrees(num), longitude: CLLocationDegrees(num2))
+                    
+                    geoCoder.reverseGeocodeLocation(location, completionHandler: {
+                            placemarks, error -> Void in
+                            
+                            // Place details
+                            guard let placeMark = placemarks?.first else { return }
+                            
+                            // Location name
+                            if let locationName = placeMark.location {
+                                print(locationName)
+                                self.localtionNameLabel.text = "Location: \(locationName)";
+                            }
+                            // Street address
+                            if let street = placeMark.thoroughfare {
+                                print(street)
+                                self.streetLabel.text = "Street: \(street)";
+                            }
+                            // City
+                            if let city = placeMark.subAdministrativeArea {
+                                print(city)
+                                self.cityLabel.text = "City: \(city)";
+                            }
+                            // Zip code
+                            if let zip = placeMark.isoCountryCode {
+                                print(zip)
+                                self.countryLabel.text = "Country code: \(zip)";
+                            }
+                            // Country
+                            if let country = placeMark.country {
+                                print(country)
+                                self.countryLabel.text = "Country: \(country)";
+                            }
+                    })
+                    
                     self.gpsCoordinates.text = json;
                     
                 }
         }
+    }
+    @IBAction func getPicture(_ sender: Any) {
+        AF.request("http://abdulrahmanuh.com:3000/socket.io/?EIO=3&transport=polling&t=MfaVmWg&sid=ec323149a929487c81dca3ff219b6e07", method: .get)
+            .responseJSON { response in
+                debugPrint(response)
+        }
+        
     }
 }
 
